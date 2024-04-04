dac2977b7ab8d3e66090c96aefaf71acde981a58
Said <dmitry@mail.ru>
2024-04-04 15:05:22 +0400

    end project

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import myphoto4 from '$lib/images/myphoto4.jpg';
+</script>
+
+
+<div class="container">
+<img alt="Моё фото 4" src={myphoto4} width="400" />
+
+<h1>Обо мне</h1>
+<p>У меня много увлечений, и я хочу рассказатьо некоторых из них:
+    <br>1. Я занимаюсь боксом. И разбираюсь в бойцах.
+    <br>2. Чтение. Люблю читать, у меня есть своя полка с книгами.
+    <br>3. Спорт. Я занимаюсь в зале. Я очень люблю качалку.
+    <br>Я расскажу ещё, но только позже  ^_^.
+</p>
+<h1> Как меня найти</h1>
+<p>Vk: @saiddd</p>
+<p>Tg: @Said241</p>
+<p>Inst: @SaiDGym</p>
+</div>
+
+<style>
+    .container {
+        margin: 50px auto;
+        max-width: 800px;
+        padding: 0 20px;
+    }
+</style>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,21 @@
+<div class="container">
+  <h1>Как меня найти</h1>
+
+  <p>Vk: @saiddd</p>
+  <p>Tg: @Said241</p>
+  <p>Inst: @SaiDGym</p>
+
+  <h1>О сайте</h1>
+  <p>Сайт создан при обучении по программе "Код будущего"</p>
+  <p>во фреймворке SvelteJS, сохранён на GitHub,</p>
+  <p>и опубликован на хостинге Netlify</p>
+  
+</div>
+
+<style>
+  .container {
+    margin: 50px auto;
+    max-width: 800px;
+    padding: 0 20px;
+  }
+</style>
